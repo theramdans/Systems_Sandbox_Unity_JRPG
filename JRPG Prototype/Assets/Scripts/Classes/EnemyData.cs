c88878cdda0127e0cc394332c8ef2bd3bda2cd74
RudyLaptop <ramdansdesu@gmail.com>
2026-07-13 15:06:16 +0700

    Add Enemy Data, preparation for enemy spawn and battle system

---
+++ JRPG Prototype/Assets/Scripts/Classes/EnemyData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class EnemyData
+{
+    public string name;
+    public int level;
+    public int MaxHP;
+    public int CurrentHP;
+    
+    public int attack;
+    public int defense;
+
+    public int ExpReward;
+}
